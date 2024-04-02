166a3d18feb8d3621b3939e8f25fcda2d2ae6aae
Krzysztof Nowakowski <klnb2014@gmail.com>
2024-04-02 18:15:26 +0200

    added getter,setter to Circle class

--- src/OOPTutorial/Circle.java
+++ src/OOPTutorial/Circle.java
@@ -11,10 +11,23 @@ class Circle {
         this.radius = radius;
         color = "red";
      }
+     public Circle(double radius, String color){
+      this.radius = radius;
+      this.color = color;
+     }
      public double getRadius(){
         return radius;
      }
      public double getArea(){
         return Math.PI*radius*radius;
+     }
+     public String getColor(){
+      return color;
+     }
+     public void setRadius(double newRadius){
+      this.radius = newRadius;
+     }
+     public void setColor(String newColor){
+      this.color = newColor;
      }
 }
