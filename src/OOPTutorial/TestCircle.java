65d0f20fc9cfc87d2f76c78f8636b1789d355b2e
Krzysztof Nowakowski <klnb2014@gmail.com>
2024-04-02 17:59:00 +0200

    added files

---
+++ src/OOPTutorial/TestCircle.java
@@ -0,0 +1,8 @@
+package OOPTutorial;
+
+public class TestCircle {
+    public static void main(String[] args){
+        Circle c1 = new Circle();
+        System.out.println("Area: "+c1.getArea()+", radius:" + c1.getRadius());
+    }
+}
