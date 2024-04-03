dd0b3cdf474601202ceb9fcfafbb0ce2aabe9883
Krzysztof Nowakowski <klnb2014@gmail.com>
2024-04-03 21:42:05 +0200

    added inheritance package

---
+++ src/Inheritance/Point2D.java
@@ -0,0 +1,39 @@
+package Inheritance;
+
+public class Point2D {
+    private float x;
+    private float y;
+    Point2D(){
+        x = 0.0f;
+        y = 0.0f;
+    }
+    Point2D(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    public float [] getXY(){
+        float[] result = new float[2]; 
+        result[0] = x;
+        result[1] = y;
+        return result;
+    }
+    public String toString(){
+        return "()"+this.x+","+this.y+")";
+    }
+}
