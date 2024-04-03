7a9f74077bc58500b414c477c4abbb5a187840fa
Krzysztof Nowakowski <klnb2014@gmail.com>
2024-04-03 21:45:31 +0200

    added TestPoint class to test Point2D and Point3D

---
+++ src/Inheritance/TestPoint.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point2D myPoint2d = new Point2D(1,2);
+        Point3D myPoint3d = new Point3D(3,2,2);
+        System.out.println(myPoint2d);
+        System.out.println(myPoint3d);
+    }
+}
